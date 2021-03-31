efc8e64f96ed8214d0ef7fae7d2d64f97f32e3d8
calipso28 <andres-severien@hotmail.de>
2021-03-31 12:15:28 +0200

    Updates

---
+++ precache-manifest.8847230b9fab1c2afea1d4d560744cc6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e9c0a815a64881076d87563355ec246",
+    "url": "/Ravenous-React-App/index.html"
+  },
+  {
+    "revision": "fb35a58eae75b447f6e9",
+    "url": "/Ravenous-React-App/static/css/main.23036f16.chunk.css"
+  },
+  {
+    "revision": "10126e3e925687b0b41e",
+    "url": "/Ravenous-React-App/static/js/2.b152fa3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ravenous-React-App/static/js/2.b152fa3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb35a58eae75b447f6e9",
+    "url": "/Ravenous-React-App/static/js/main.87d3b23c.chunk.js"
+  },
+  {
+    "revision": "6f2e18dda849a3dc941b",
+    "url": "/Ravenous-React-App/static/js/runtime-main.0c2d2205.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Ravenous-React-App/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Ravenous-React-App/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
